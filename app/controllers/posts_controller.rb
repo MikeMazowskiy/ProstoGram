51ba7222ff31c7873d540f9b9785caa82271505d
Mike_Mazovskiy <vairelviper@gmail.com>
2021-07-04 22:57:30 +0200

    Creatable posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,15 @@
 class PostsController < ApplicationController
-  def index
+  def create
+    Post.create(post_params)
+    redirect_to root_path
+  end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:description, :image, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   devise_for :users
   get 'posts/index'
 
   root to: 'posts#index'
+  resources :posts, only: [:new, :create]
   resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
