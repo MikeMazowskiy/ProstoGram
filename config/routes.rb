50044be6fe9bacb928dd28e41a17e7689a7df5c1
Mike_Mazovskiy <vairelviper@gmail.com>
2021-07-04 17:49:01 +0200

    Add log out and connected amazon s3 for upload photo and create post model

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'posts/index'
 
   root to: 'posts#index'
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
