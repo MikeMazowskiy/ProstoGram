be34ba1b871977dfae92d42161fa4980cdd0a961
Mike_Mazovskiy <vairelviper@gmail.com>
2021-07-08 10:53:01 +0200

    Add comments and create likes

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    @comment.user_id = current_user.id if user_signed_in?
+
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      redirect_to home_index_path
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :post_id, :comment)
+  end
+end
